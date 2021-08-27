294ea4e2f1c23fd60a27a841409554022f3c2182
Theo <lucastheo.g.a@gmail.com>
2021-08-27 00:05:11 -0300

    Add milistone 3 nova forma de coleta

---
+++ ethereum network local/run/milestone_3/v2_array.js
@@ -0,0 +1,77 @@
+module.exports = async function(callback) {
+    web3.eth.defaultAccount = ( await ( web3.eth.personal.getAccounts() ) )[0];
+    console.log("Inicio")
+    try{
+        
+        _deploy_milestone_3_Array = await artifacts.require("Milestone3Array").deployed();
+        
+        var i, j;
+        for( i = 0; i < 500; i++ ){
+            let resultados = Array()
+            for( j = 0; j < 20; j++ ){
+                resultados.push( await run() )
+            }
+            console.log("execução de numero: " + i )
+            write( "../results/milestone_3/crude/v2_array/" + i + ".dat" , resultados );
+        }
+    }catch (error) {
+        console.log(error)
+    }
+    console.log("fim")
+}
+
+async function run(){
+    var result = Array()
+    var estagio_1 = {}
+    
+    estagio_1.result = await inserirElemento()
+    estagio_1.function_name = "inserirElemento"
+    result.push( estagio_1 )
+
+    var estagio_2 = {}
+    estagio_2.result = await contarElemento()
+    estagio_2.function_name = "contarElemento"
+    result.push( estagio_2 )
+
+    var estagio_3 = {}
+    estagio_3.result = await percorre()
+    estagio_3.function_name = "percorre"
+    result.push( estagio_3 )
+
+    var estagio_4 = {}
+    estagio_3.result = await percorreConta()
+    estagio_4.function_name = "percorreConta"
+    result.push( estagio_3 )
+
+    return result
+}
+
+async function write( path , json_result ){
+    const fs = require('fs')
+
+    fs.writeFile( path, JSON.stringify(json_result, null , 2) , (err) => {
+        if (err) throw err;
+    })
+}
+
+async function inserirElemento(){
+    var flag = Math.random() > 0.5 ? true : false 
+    await _deploy_milestone_3_Array.inserirElemento(flag)
+    return flag
+}
+
+async function contarElemento(){
+    var result;
+    await _deploy_milestone_3_Array.contaElemento()
+    return null
+}
+
+async function percorre(){
+    await _deploy_milestone_3_Array.percorre()
+    return null
+}
+
+async function percorreConta(){
+    await _deploy_milestone_3_Array.percorreConta()
+    return null
+}
